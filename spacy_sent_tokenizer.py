7fa8ab3809339422f43f18645d4f19573407dabc
maggieezzat <maggieezzat9@gmail.com>
2019-07-15 14:10:01 +0200

    spacy fix

--- spacy_sent_tokenizer.py
+++ spacy_sent_tokenizer.py
@@ -93,6 +93,7 @@ def tokenize_into_sents(rootdir=dir, output_root=out_dir):
                                 clean_sent = ' '.join(clean_sent.split())
                                 new_file.write(clean_sent + '\n')
                             new_file.write('\n')
+                            doc = ""
 
                         else:
                             doc = doc + content[i]
